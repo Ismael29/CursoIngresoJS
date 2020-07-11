738dd5af74328bb819a3e70bd34526e417a7d60d
Ismael29 <ismaelspallettauniversidad@hotmail.com>
2020-07-11 09:54:08 -0300

    Realizado

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,19 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
+
+
+function mostrar() 
 {
-	alert("ok");
+var mensaje;
+var opcion = prompt("Introduzca su mensaje:");
+ 
+if (opcion == null || opcion == "") {
+        mensaje = "Canselaste o el mensaje esta vacío";
+        } else {
+            mensaje = "Su Mensaje es  " + opcion;
+			}
+alert(" " + mensaje);					
 }
+
 
