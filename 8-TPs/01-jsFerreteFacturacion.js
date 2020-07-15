96ded30f6a9f0d5da0e2884a6b66a552f1ac67a8
Ismael29 <ismaelspallettauniversidad@hotmail.com>
2020-07-14 21:53:34 -0300

    Realizado

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,74 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+var precioUno;
+var precioDos;                         // variables
+var precioTres;
+var precioFinal;
+var precioUno=txtIdPrecioUno.value;             // toma el valor ingresador por id 
+var precioDos=txtIdPrecioDos.value;
+var precioTres=txtIdPrecioTres.value;
+
+precioUno=parseInt(precioUno);  
+precioDos=parseInt(precioDos);                  // tranforma todo a int numero
+precioTres=parseInt(precioTres);
+precioFinal=(precioUno+precioDos+precioTres);   // suma de todos los precios
+
+
+alert("La suma de sus precios ingresador es: "+precioFinal);  // pasa la suma de los precios por alert
+
 }
-function Promedio () 
+
+
+
+function Promedio ()
 {
-	
+var precioUno;      
+var precioDos;                                      // variables
+var precioTres;
+var precioFinal;
+var precioUno=txtIdPrecioUno.value;                 // toma el valor ingresador por id 
+var precioDos=txtIdPrecioDos.value;
+var precioTres=txtIdPrecioTres.value;
+
+precioUno=parseInt(precioUno);  
+precioDos=parseInt(precioDos);
+precioTres=parseInt(precioTres);
+precioFinal=((precioUno+precioDos+precioTres)/3);          // suma todos lso precios y los divide por la misma cantidad para saar promedio
+
+
+alert("El promedio de sus tres precios es: "+precioFinal);   // pasa promedio pr alert
+    
 }
+
+
 function PrecioFinal () 
 {
-	
+var precioUno;
+var precioDos;
+var precioTres;                                                     // variables
+var precioFinal;
+var precioAgregado;
+var precioUno=txtIdPrecioUno.value;                         // pasa el valor pasado por id 
+var precioDos=txtIdPrecioDos.value;
+var precioTres=txtIdPrecioTres.value;
+    
+precioUno=parseInt(precioUno);                      // transforma a num int
+precioDos=parseInt(precioDos);
+precioTres=parseInt(precioTres);
+precioFinal=(precioUno+precioDos+precioTres);               // suma los 3 preios ingresados
+precioFinal=parseInt(precioFinal);
+precioAgregado=(precioFinal*21/100);   // precioAgregado=(precioFinal*0.21);  // saca el 21 % de lso numeros ingresados
+precioAgregado=parseInt(precioAgregado);              // transforma a int numero 
+   
+
+
+alert("La suma de sus precios ingresados mas 21 % de IVA es: "+(precioFinal+precioAgregado));  // suma el total de precios mas el 21% de dicho monto 
+    
+
+
+
+
+
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,59 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo;
+    var ancho;                                      // declaracion de variables
+    var cantidadDeHilosDeAlambre;
 
+    var largo=txtIdLargo.value;
+    var ancho=txtIdAncho.value;                       // se toma el valor por el id 
+
+    largo=parseInt(largo);
+    ancho=parseInt(ancho);
+
+    cantidadDeHilosDeAlambre=((2*(largo+ancho))*3);         // se saca el perimetro de un rectangulo y se multiplica por 3
+
+    alert("La cantidad de hilos que necesitas es: "+cantidadDeHilosDeAlambre); // mostrar por alert
 }
+
+
 function Circulo () 
 {
-	
+    var radio;
+    var cantidadDeHilosDeAlambre;                  //variables
+    var radio=txtIdRadio.value;
+    radio=parseInt(radio);                                      // transforma a int numero
+
+    cantidadDeHilosDeAlambre=(Math.pow(3.14*radio,2))*3;         // circunferencia de un circulo * 3 
+
+    alert("La cantidad de hilos que necesitas es: "+cantidadDeHilosDeAlambre );   // mostrar por alert
 }
+
+
 function Materiales () 
 {
-	
+    var medida1;
+    var medida2;                    // variables
+    var bolsaCemento;
+    var bolsaCal;
+
+    var bolsaCemento=2;              // valores fijos de cemento y cal 
+    var bolsaCal=3;
+    var medida1=txtIdLargo.value;
+    var medida2=txtIdAncho.value;     // se toma el valor por el id
+
+    medida1=parseInt(medida1);          // transforma a int numero
+    medida2=parseInt(medida2);
+
+    alert("La cantidad de bolsas de cemento que necesitas son:  "+(medida1*medida2)*bolsaCemento); // 
+    alert("La cantidad de bolsas de cal que necesitas son:  "+ (medida1*medida2)*bolsaCal);
+
+
+
+
+    
+
+
+
+
 }
